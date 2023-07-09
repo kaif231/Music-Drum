03afe2cb0c23d96df38c44501b6a5966b01be768
kaif231 <120895330+kaif231@users.noreply.github.com>
2023-07-09 18:32:25 +0530

    live

---
+++ music.js
@@ -0,0 +1,66 @@
+//
+//Storing the Butttons in variables
+let m1=document.getElementById('m1')
+let m2=document.getElementById('m2')
+let m3=document.getElementById('m3')
+let m4=document.getElementById('m4')
+let m5=document.getElementById('m5')
+let m6=document.getElementById('m6')
+let m7=document.getElementById('m7')
+let m8=document.getElementById('m8')
+let m9=document.getElementById('m9')
+//
+m1.onclick=()=>{
+    document.body.style.background ='red';
+    let e1=new Audio('E/E1.m4a')
+    e1.play()
+}
+//
+m2.onclick=()=>{
+    document.body.style.background ='black';
+    document.body.style.color ='white';
+    let e2=new Audio('E/E2.m4a')
+    e2.play()
+}
+//
+m3.onclick=()=>{
+    document.body.style.background ='grey';
+    let e3=new Audio('E/E3.m4a')
+    e3.play()
+}
+//
+m4.onclick=()=>{
+    document.body.style.background ='brown';
+    let e4=new Audio('E/E4.m4a')
+    e4.play()
+}
+//
+m5.onclick=()=>{
+    document.body.style.background ='pink';
+    let e5=new Audio('E/E5.m4a')
+    e5.play()
+}
+//
+m6.onclick=()=>{
+    document.body.style.background ='green';
+    let e6=new Audio('E/E6.m4a')
+    e6.play()
+}
+//
+m7.onclick=()=>{
+    document.body.style.background ='blue';
+    let e7=new Audio('E/E7.m4a')
+    e7.play()
+}
+//
+m8.onclick=()=>{
+    document.body.style.background ='purple';
+    let e8=new Audio('E/E8.m4a')
+    e8.play()
+}
+//
+m9.onclick=()=>{
+    document.body.style.background ='yellow';
+    let e9=new Audio('E/E9.m4a')
+    e9.play()
+}
